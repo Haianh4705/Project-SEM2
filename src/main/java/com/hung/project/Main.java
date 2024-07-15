1310ab722e790e5f42e1ad1cf10044f9d8397af0
nguyendoanhung <nguyendoanhungcv@gmail.com>
2024-07-15 18:30:50 +0700

    first commit

---
+++ src/main/java/com/hung/project/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hung.project;
+
+import static com.hung.project.Users.cipher;
+import java.util.Base64;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+}
